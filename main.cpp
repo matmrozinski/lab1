4e3366cb4e31637a4299d72c2d17cde3b68cfe5d
matmrozinski <m.mrozinski123@gmail.com>
2016-04-28 18:11:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>
+
 
 char **charSquare(int n);
 void drawCharSquare(char **square, int n);
 
+using namespace std;
+
 int main(int argc, char **argv)
 {
 	int n;
-	drawCharSquare(charSquare(n), n);	
+	drawCharSquare(charSquare(n), n);
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square=NULL;
+	square= new char*[n];
+
+	for(int i=0;i<n;i++)
+	{
+		square[i]=new char[n];
+	}
+
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+			square[i][j]='a'+	rand()%('z'-'a');
+
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
-
+for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+            cout << square[i][j] << " ";
+            cout << endl;
 }
